80cb04ba72792be2fa8b532667d53c8b548575f8
winograde@upmc.edu <winograde@upmc.edu>
2015-02-18 08:14:42 -0500

    wip

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -6,12 +6,15 @@ group{'devs':
 	ensure => present
 }
 
+$password = hiera('password::user')
+
 user{"ethan":
 	ensure => present,
 	shell => '/bin/bash',
 	home => '/home/ethan',
 	gid => 'devs',
 	managehome => true,
+	 password => generate('/bin/sh', '-c', "mkpasswd -m sha-512 ${password} | tr -d '\n'"),
 	require => [Group['devs']]
 }
 
@@ -29,13 +32,9 @@ Exec{
 exec{'apt-get update':}
 exec{'gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3':}
 
-package {'wget':
-	ensure => installed
-}
-
-package {'unzip':
-	ensure => installed
-}
+package {'wget': ensure => installed}
+package {'unzip': ensure => installed}
+package {'terminator': ensure => installed}
 
 class {'rvm_wrapper':
   require => [Package['curl'],Exec['gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3']] 
@@ -55,4 +54,11 @@ gvm::package { 'springboot':
 
 class { 'springtoolsuite':
 	require => [Package['curl']]
+}
+
+class { 'karaf':
+  user => 'ethan',
+  karafVersion => '3.0.3',
+  tmpDir => '/tmp',
+  require => [User['ethan']]
 }
